f4ded7b5da9fceddfa8e60796ca2764484e4f6e2
Anshul Patel <anshulpatel5757@gmail.com>
2023-07-31 16:19:59 +0530

    added react-routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
-import Counter from "./features/counter/Counter";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 import "./App.css";
+import Counter from "./features/counter/Counter";
 import Home from "./pages/Home";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignupPage />,
+  },
+]);
+
 export default function App() {
-  return (
-    <div>
-      <SignupPage />
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
